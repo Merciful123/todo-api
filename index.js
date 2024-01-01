176a52d49b9d58031a15c2e9f425d439fb6d0b7e
Aamir Raza <merciful9795@gmail.com>
2024-01-01 22:06:47 +0530

    mysql db connected on digitalocean

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 import express from "express"
 import mysql from "mysql";
 import cors from "cors";
+import dotenv from "dotenv"
 
 const app = express()
+dotenv.config();
 
-const port = 8000
+const port = process.env.PORT
 
 app.use(express.json())
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 
 const db = mysql.createPool({
-    host: "localhost",
-    user:"root",
-    password: "Mysql@313",
-    database: "todo-test",
-    
-})
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "Mysql@313",
+  //   database: "todo-test",
+
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+
 
 // get all todo 
 
